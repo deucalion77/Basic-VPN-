830026a6c0bc48b0cce94b6211183825ae4ed457
k8admin <uthmika13@gmail.com>
2024-12-20 15:57:06 +0530

    vpn files adeded

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,12 @@ RUN apt update && apt install -y python3 python3-pip && apt clean
 
 RUN apt install -y net-tools 
 
-RUN apt install -y iputils-ping
+RUN apt install -y iputils-ping iproute2 && apt clean
 
-CMD ["sleep", "infinity"]
+RUN mkdir -p /dev/net && \
+    mknod /dev/net/tun c 10 200 && \
+    chmod 600 /dev/net/tun
+
+#RUN python3 vpn/vpn.py
+
+CMD ["python3", "vpn/vpn.py"]
---
+++ vpn1.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python3
+
+import socket
+import os
+import struct
+import subprocess
+from fcntl import ioctl
+
+TUNSETIFF =0x400454ca
+IFF_TUN = 0x0001
+IFF_NO_PI = 0x1000
+
+def open_tun(devname):
+    fd = os.open('/dev/net/tun', os.O_RDWR)
+    ifr = struct.pack("16sH", devname, IFF_TUN | IFF_NO_PI)
+    ifs = ioctl(fd, TUNSETIFF, ifr)
+    return fd
+
+
+def ip_add(devname, ip_address):
+    subprocess.run(['ip', 'addr', 'add', ip_address, 'dev', devname], check=True)
+    subprocess.run(['ip', 'link', 'set', 'dev', devname, 'up'], check=True)
+fd = open_tun(b'asa0')
+
+ip_add('asa0', '10.10.1.1/24')
+
+while True:
+    buffer = os.read(fd, 1600)
+    print(buffer)
